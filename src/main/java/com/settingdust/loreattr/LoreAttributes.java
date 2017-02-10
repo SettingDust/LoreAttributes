89e6e29f487f86f5880c14e62b75df098399b948
Jsettingdust <settingdust@gmail.com>
2017-02-10 20:24:38 +0800

    delete rune

--- src/main/java/com/settingdust/loreattr/LoreAttributes.java
+++ src/main/java/com/settingdust/loreattr/LoreAttributes.java
@@ -5,9 +5,6 @@ import com.settingdust.loreattr.attribute.AttributesManager;
 import com.settingdust.loreattr.attribute.handler.LoreHandler;
 import com.settingdust.loreattr.command.Names;
 import com.settingdust.loreattr.command.handler.CommandHandler;
-import com.settingdust.loreattr.gui.item.Item;
-import com.settingdust.loreattr.gui.rune.handler.GuiHandler;
-import com.settingdust.loreattr.gui.rune.util.RuneUtils;
 import com.settingdust.loreattr.util.LanguageUtils;
 import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.bukkit.event.HandlerList;
@@ -28,22 +25,18 @@ public class LoreAttributes extends JavaPlugin {
 
         LanguageUtils.init();
         AttributesManager.init();
-        RuneUtils.init();
 
         ConfigurationSerialization.registerClass(Attribute.class);
-        ConfigurationSerialization.registerClass(Item.class);
     }
 
     @Override
     public void onEnable() {
         this.getCommand(Names.LOREATTR.getName()).setExecutor(new CommandHandler());
         this.getServer().getPluginManager().registerEvents(new LoreHandler(this), this);
-        this.getServer().getPluginManager().registerEvents(new GuiHandler(), this);
     }
 
     public void onDisable() {
         HandlerList.unregisterAll(this);
         AttributesManager.disable();
-        RuneUtils.save();
     }
 }
